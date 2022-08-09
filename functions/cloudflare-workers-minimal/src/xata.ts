40a15edf28285fdc8fe4e7f873861a549af2cdc3
Atila Fassina <atila@fassina.eu>
2022-08-09 17:26:40 +0200

    add release and format tasks

--- functions/cloudflare-workers-minimal/src/xata.ts
+++ functions/cloudflare-workers-minimal/src/xata.ts
@@ -5,30 +5,30 @@ import {
   RestRespositoryFactory,
   XataClientOptions,
   XataRecord,
-} from "@xata.io/client";
+} from '@xata.io/client'
 
 export interface Url extends XataRecord {
-  destination?: string | null;
-  slug?: string | null;
-  ownerIp?: string | null;
+  destination?: string | null
+  slug?: string | null
+  ownerIp?: string | null
 }
 
-const links = { urls: [] };
+const links = { urls: [] }
 
 export class XataClient extends BaseClient<{
-  urls: Repository<Url>;
+  urls: Repository<Url>
 }> {
   constructor(options: XataClientOptions) {
     super(
       {
-        databaseURL: "https://url-shortener-u7o0j6.xata.sh/db/xata-short",
+        databaseURL: 'https://url-shortener-u7o0j6.xata.sh/db/xata-short',
         ...options,
       },
-      links
-    );
-    const factory = options.repositoryFactory || new RestRespositoryFactory();
+      links,
+    )
+    const factory = options.repositoryFactory || new RestRespositoryFactory()
     this.db = {
-      urls: factory.createRepository(this, "urls"),
-    };
+      urls: factory.createRepository(this, 'urls'),
+    }
   }
 }
