723576cb422fa844d336c5d69480c06a618b3bd7
Atila Fassina <atila@fassina.eu>
2022-09-05 20:58:13 +0200

    adjust css

--- apps/remix-minimal/app/root.tsx
+++ apps/remix-minimal/app/root.tsx
@@ -17,7 +17,7 @@ export const links: LinksFunction = () => {
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
   viewport: 'width=device-width,initial-scale=1',
-  ...metatags,
+  ...metatags(),
 })
 
 export default function App() {
