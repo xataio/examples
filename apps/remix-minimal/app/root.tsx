efbf6dca7f67f89b0b4d579b93cd1fb6a024fed9
Atila Fassina <atila@fassina.eu>
2022-09-05 20:42:53 +0200

    add og:image and security headers

--- apps/remix-minimal/app/root.tsx
+++ apps/remix-minimal/app/root.tsx
@@ -8,14 +8,16 @@ import {
   ScrollRestoration,
 } from '@remix-run/react'
 import styles from '~/styles.css'
+import { metatags } from './lib/settings'
+
 export const links: LinksFunction = () => {
   return [{ rel: 'stylesheet', href: styles }]
 }
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
-  title: 'Remix with Xata',
   viewport: 'width=device-width,initial-scale=1',
+  ...metatags,
 })
 
 export default function App() {
