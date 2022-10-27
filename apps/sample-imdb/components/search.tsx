95b873e0063cc016a6d58250254fe218897b8550
Atila Fassina <atila@fassina.eu>
2022-10-27 03:35:43 -0700

    sample(start): xmdb

---
+++ apps/sample-imdb/components/search.tsx
@@ -0,0 +1,3 @@
+export const Search = () => {
+  return <div className="h-10 bg-white"></div>
+}
---
+++ apps/sample-imdb/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
