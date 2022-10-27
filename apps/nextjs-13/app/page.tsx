9dd561d38874a5ed93caef7c2c8b8a021fe4bcca
Alexis Rico <sferadev@gmail.com>
2022-10-27 11:11:42 +0200

    Move to async/await
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- apps/nextjs-13/app/page.tsx
+++ apps/nextjs-13/app/page.tsx
@@ -1,12 +1,11 @@
 import Link from 'next/link';
-import { use } from 'react';
 import { getXataClient } from '../services/xata';
 import styles from './page.module.css';
 
 const xata = getXataClient();
 
-export default function Home() {
-  const data = use(xata.db.drivers.getAll());
+export default async function Home() {
+  const data = await xata.db.drivers.getAll();
 
   return (
     <div className={styles.container}>
