a8ca96ad339f0dba066581284d0a96a12df05997
Fabien BERNARD <fabien0102@gmail.com>
2022-11-23 12:25:26 +0100

    Use vercel adapter

--- apps/starter-astro/astro.config.mjs
+++ apps/starter-astro/astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import vercel from '@astrojs/vercel/serverless'
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  output: 'server',
+  adapter: vercel(),
   integrations: [react()],
-});
+})
