b8e60acfebedde69da20011cf109ee3bc6c2dd74
Alexis Rico <sferadev@gmail.com>
2023-05-04 20:51:52 +0200

    Update apps/starter-nextjs-rsc-actions/components/waitlist.tsx
    Co-authored-by: Atila Fassina <atila@fassina.eu>

--- apps/starter-nextjs-rsc-actions/components/waitlist.tsx
+++ apps/starter-nextjs-rsc-actions/components/waitlist.tsx
@@ -15,7 +15,7 @@ export default function Waitlist() {
     <form action={create} className="flex gap-6 mb-6">
       <input
         type="email"
-        id="email"
+        name="email"
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder="picard@enterprise.com"
         required
