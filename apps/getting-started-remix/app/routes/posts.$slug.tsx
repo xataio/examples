2375f4319afb19fe1067c71f43bb4ca6a783ba9f
Phil Leggetter <phil@leggetter.co.uk>
2023-08-14 19:15:40 +0100

    feat: Xata + Remix getting started app

---
+++ apps/getting-started-remix/app/routes/posts.$slug.tsx
@@ -0,0 +1,36 @@
+import { LoaderArgs, V2_MetaFunction, json } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+import { getXataClient } from 'src/xata'
+
+export const meta: V2_MetaFunction<typeof loader> = ({ data }) => {
+  return [{ title: `${data?.post?.title} - Xata and Remix` }]
+}
+
+export async function loader({ params }: LoaderArgs) {
+  const xata = getXataClient()
+
+  const post = await xata.db.Posts.filter({ slug: params.slug }).getFirst()
+
+  return json({
+    post,
+  })
+}
+
+export default function Post() {
+  const { post } = useLoaderData<typeof loader>()
+
+  return (
+    <div className="w-full max-w-5xl mt-16">
+      <p className="mb-2">
+        <a href="/" className="text-purple-600">
+          &larr; Back to blog
+        </a>
+      </p>
+      <h1 className="text-3xl mb-2">{post?.title}</h1>
+      <p className="text-sm mb-4 text-purple-950 dark:text-purple-200">
+        {new Date(post?.pubDate ?? '').toDateString()}
+      </p>
+      <p className="text-xl">{post?.description}</p>
+    </div>
+  )
+}
