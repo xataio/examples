70c434e87b194f4ecb0b937ad4d75b73d37a4cf6
Phil Leggetter <phil@leggetter.co.uk>
2023-08-15 13:44:31 +0100

    chore: Astro/GS remove tailwind darkmode flag

--- apps/getting-started-astro/tailwind.config.cjs
+++ apps/getting-started-astro/tailwind.config.cjs
@@ -1,8 +1,7 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
