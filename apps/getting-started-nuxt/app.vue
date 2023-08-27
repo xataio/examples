ed252865f351022bb7ecd7b957a45f1a94cefaba
Phil Leggetter <phil@leggetter.co.uk>
2023-08-27 18:21:16 +0100

    chore: Nuxt/GS update <main /> CSS

--- apps/getting-started-nuxt/app.vue
+++ apps/getting-started-nuxt/app.vue
@@ -4,8 +4,12 @@ useHead({
 });
 </script>
 <template>
-  <main class="flex flex-wrap justify-center content-start p-8 lg:p-24 min-h-screen">
-    <div class="z-10 h-50 w-full max-w-5xl items-center justify-between text-xl lg:flex">
+  <main
+    class="flex flex-col justify-center items-center p-8 lg:p-24 min-h-screen"
+  >
+    <div
+      class="z-10 h-50 w-full max-w-5xl items-center justify-between text-xl lg:flex"
+    >
       <p
         class="fixed left-0 top-0 flex w-full justify-center pb-6 pt-8 lg:static lg:w-auto bg-gradient-to-b from-white via-white via-65% dark:from-black dark:via-black lg:bg-none"
       >
@@ -15,7 +19,9 @@ useHead({
         class="fixed bottom-0 left-0 flex w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none"
       >
         <a href="https://xata.io" class="w-20">
-          <img src="https://raw.githubusercontent.com/xataio/examples/main/docs/app_logo.svg" />
+          <img
+            src="https://raw.githubusercontent.com/xataio/examples/main/docs/app_logo.svg"
+          />
         </a>
       </div>
     </div>
