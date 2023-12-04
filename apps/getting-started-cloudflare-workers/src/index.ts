4a613cb2a4cfc29f2233c795002cdaa91865aa0e
Emily <emily.morgan@code.berlin>
2023-12-04 11:15:32 +0100

    feat/getting-started-cloudflare-workers

---
+++ apps/getting-started-cloudflare-workers/src/index.ts
@@ -0,0 +1,27 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+import { XataClient } from './xata';
+
+export interface Env {
+	XATA_BRANCH: string;
+	XATA_API_KEY: string;
+}
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const xata = new XataClient({
+			apiKey: env.XATA_API_KEY,
+			branch: env.XATA_BRANCH,
+		});
+		const posts = await xata.db.Posts.getAll()
+		return new Response(`Total Posts: ${posts.length}`);
+	},
+};
