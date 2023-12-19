b0ef04a3d4ab25c176e51f8426565bae6dd616ca
Dave Snider <dave.snider@gmail.com>
2023-12-19 09:25:15 -0500

    Remove unmaintained starters

--- apps/starter-nextjs-trpc-clerk/components/TodoList.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export function TodoList({ children }: React.PropsWithChildren) {
-  return <ul>{children}</ul>;
-}
--- apps/starter-solidstart/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import solid from 'solid-start/vite'
-import { defineConfig } from 'vite'
-import vercel from 'solid-start-vercel'
-
-export default defineConfig(() => {
-  return { plugins: [solid({ adapter: vercel() })] }
-})
