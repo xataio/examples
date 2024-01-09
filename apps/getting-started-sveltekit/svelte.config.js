05ebb6972f9925495e5232f847b151e18052b0da
Dave Snider <dave.snider@gmail.com>
2024-01-09 12:30:33 -0500

    Upgrade example to sveltekit v2 (#609)
    * Upgrade example to sveltekit v2
    
    * lock

--- apps/getting-started-sveltekit/svelte.config.js
+++ apps/getting-started-sveltekit/svelte.config.js
@@ -1,11 +1,11 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: [vitePreprocess({})],
+  preprocess: vitePreprocess(),
 
   kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
