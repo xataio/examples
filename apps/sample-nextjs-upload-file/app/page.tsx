ff0b4aae81f62141bd0a7361f7abefe519952c60
Alexis Rico <sferadev@gmail.com>
2024-01-19 14:39:24 +0100

    Updates
    Signed-off-by: Alexis Rico <sferadev@gmail.com>

--- apps/sample-nextjs-upload-file/app/page.tsx
+++ apps/sample-nextjs-upload-file/app/page.tsx
@@ -1,9 +1,34 @@
 import { Upload } from '@/components/upload';
-import { ImagesRecord, getXataClient } from '../utils/xata';
+import { getXataClient } from '@/utils/xata';
+
+const xata = getXataClient();
+
+const createImage = async (mediaType: string) => {
+  'use server';
+
+  // Create an empty image record with no base64 content
+  const record = await xata.db.images.create(
+    { image: { mediaType, base64Content: '' } },
+    // Request an uploadUrl from the created record. We can use it to upload a file to replace the empty one
+    ['*', 'image.uploadUrl']
+  );
+
+  return { id: record.id, uploadUrl: record.image?.uploadUrl };
+};
+
+const deleteImage = async (id: string) => {
+  'use server';
+  await xata.db.images.delete(id);
+};
 
 export default async function Home() {
-  const xata = getXataClient();
   const images = await xata.db.images.getMany();
-  const serializedImages = images.toSerializable();
-  return <Upload images={serializedImages} />;
+
+  return (
+    <Upload
+      images={images.toSerializable()}
+      createImage={createImage}
+      deleteImage={deleteImage}
+    />
+  );
 }
--- apps/sample-nextjs-upload-file/components/upload.tsx
+++ apps/sample-nextjs-upload-file/components/upload.tsx
@@ -1,14 +1,26 @@
 'use client';
-import { FC, FormEvent, useState } from 'react';
-import { Images, ImagesRecord } from '@/utils/xata';
+
+import { ImagesRecord } from '@/utils/xata';
 import { JSONData } from '@xata.io/client';
+import { FC, FormEvent, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 interface UploadProps {
   images: JSONData<ImagesRecord>[];
+  createImage: (
+    mediaType: string
+  ) => Promise<{ id: string; uploadUrl?: string }>;
+  deleteImage: (id: string) => Promise<void>;
 }
 
-export const Upload: FC<UploadProps> = ({ images }) => {
-  const [file, setFile] = useState(null);
+export const Upload: FC<UploadProps> = ({
+  images,
+  createImage,
+  deleteImage
+}) => {
+  const router = useRouter();
+
+  const [file, setFile] = useState<File>();
   const [message, setMessage] = useState('');
   const [isUploading, setIsUploading] = useState(false);
 
@@ -20,56 +32,31 @@ export const Upload: FC<UploadProps> = ({ images }) => {
       return;
     }
 
-    // Grab the form data
-    const formData = new FormData();
-    const fileObj = file as File;
-    formData.append('fileType', fileObj.type);
-
-    try {
-      // This route creates new image and tag records in Xata
-      // If you look at the api route code, you'll see that we're not actually
-      // uploading the image here. Instead, we're creating a record in the database
-      // with a temporary, empty image. We do this because we need to generate a
-      // pre-signed URL for the image upload.
-      const response = await fetch('/api/image', {
-        method: 'POST',
-        body: formData
-      });
-      if (response.status !== 200) {
-        throw new Error("Couldn't create image record");
-      }
-
-      const record = await response.json();
-
-      // The response include a pre-signed uploadUrl on the record. Below, we then send a file
-      // directly to Xata on the client side using the PUT request. This lets us upload
-      // large files that would otherwise exceed the limit for serverless functions on
-      // services like Vercel.
-      if (response.status === 200) {
-        setIsUploading(true);
-        try {
-          setIsUploading(true);
-          await fetch(record.image.uploadUrl, { method: 'PUT', body: file });
-          setMessage('Your image was uploaded successfully. Refresh the page to see it.');
-          setIsUploading(false);
-        } catch (error) {
-          // Delete the record and associated tag
-          await fetch(`/api/image?imageId=${record.id}`, { method: 'DELETE' });
-          setMessage("Couldn't upload image");
-          setIsUploading(false);
-          throw new Error("Couldn't upload image");
-        }
-      } else {
-        setMessage("Couldn't upload image");
-        throw new Error("Couldn't upload image");
-      }
-    } catch (error) {
-      setMessage('An error occurred while uploading the image.');
+    // This route creates new image and tag records in Xata
+    // If you look at the api route code, you'll see that we're not actually
+    // uploading the image here. Instead, we're creating a record in the database
+    // with a temporary, empty image. We do this because we need to generate a
+    // pre-signed URL for the image upload.
+    const { id, uploadUrl } = await createImage(file.type);
+    if (!uploadUrl) {
+      throw new Error("Couldn't create image record");
     }
-  };
 
-  const handleFileChange = (e: any) => {
-    setFile(e.target.files[0]);
+    // The response include a pre-signed uploadUrl on the record. Below, we then send a file
+    // directly to Xata on the client side using the PUT request. This lets us upload
+    // large files that would otherwise exceed the limit for serverless functions on
+    // services like Vercel.
+    try {
+      setIsUploading(true);
+      await fetch(uploadUrl, { method: 'PUT', body: file });
+    } catch (error) {
+      // Delete the record if the upload fails
+      await deleteImage(id);
+      setMessage("Couldn't upload image");
+    } finally {
+      setIsUploading(false);
+      router.refresh();
+    }
   };
 
   return (
@@ -81,21 +68,36 @@ export const Upload: FC<UploadProps> = ({ images }) => {
           id="file"
           type="file"
           name="file"
-          accept="image/png, image/jpeg"
-          onChange={handleFileChange}
+          accept="image/*"
+          onChange={(event) => {
+            setFile(event.target.files?.[0]);
+          }}
         />
         <p>{message}</p>
         <button type="submit" disabled={isUploading}>
           Upload
         </button>
       </form>
+
       {images.length > 0 && (
         <>
           <h2>Images</h2>
           <ul>
             {images.map((image) => (
-              <li key={image.id} style={{ maxWidth: 200}}>
-                <img src={image.image?.url} width="100%" alt={image.image?.name} />
+              <li key={image.id} style={{ maxWidth: 200 }}>
+                <img
+                  src={image.image?.url}
+                  width="100%"
+                  alt={image.image?.name}
+                />
+                <button
+                  onClick={() => {
+                    deleteImage(image.id);
+                    router.refresh();
+                  }}
+                >
+                  Delete
+                </button>
               </li>
             ))}
           </ul>
